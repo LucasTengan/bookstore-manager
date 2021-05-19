040d080943069da3d59e2fae68ceca81ca4b8d0e
Lucas Tengan <lucastengan@usp.br>
2021-05-19 11:07:12 -0300

    First commit: project structure created

---
+++ src/test/java/br/com/lucas/bookstoremanager/BookstoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
